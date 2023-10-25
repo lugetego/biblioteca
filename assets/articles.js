4c2e916cbc789fa1fca8da136fefaaf6f75a5416
lugetego <lugetego123./>
2023-10-24 18:24:50 -0600

    Artículos js en index site

--- assets/articles.js
+++ assets/articles.js
@@ -52,7 +52,7 @@ function populateArticles(articles) {
 
 async function logArticles() {
     try {
-        const response = await fetch("https://gaspacho.matmor.unam.mx/SRBexport/app_dev.php/export/2023/5", {
+        const response = await fetch("https://gaspacho.matmor.unam.mx/SRBexport/app_dev.php/export/5", {
             mode: "cors",
             headers: {
                 accept: 'application/json',
